36124c142052bc32cf90e1fb186c530bba16cde9
taylor-ben <benjamin.taylor@mail.huji.ac.il>
2018-05-16 12:31:53 +0300

    navbar

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>My React App</h1>
       </div>
     );
