329645b290161f4ec51ad4127c01cc76f50d144c
Ben Taylor <37868849+taylor-ben@users.noreply.github.com>
2018-06-10 10:46:06 +0300

    Delete keys.ts

--- src/config/keys.ts
+++
@@ -1,4 +0,0 @@
-
-// export const mongoURI = 'mongodb://ben:0506912211@ds017776.mlab.com:17776/devconnector';
-export const mongoURI = 'mongodb://ben:0506912211@ds219130.mlab.com:19130/libberdb';
-export const secretOrKey = 'secret';
